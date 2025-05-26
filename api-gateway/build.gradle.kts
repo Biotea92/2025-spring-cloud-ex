f04f18f60202543dfa9973022bdb0696e2ef6c50
kimwoojong <skekdnwhd@gmail.com>
2025-05-26 16:18:23 +0900

    micrometer-tracing, zipkin 추가

--- api-gateway/build.gradle.kts
+++ api-gateway/build.gradle.kts
@@ -2,4 +2,9 @@ dependencies {
     implementation("org.springframework.cloud:spring-cloud-starter-gateway")
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
     implementation("org.springframework.cloud:spring-cloud-starter-config")
+    
+    // Micrometer Tracing 의존성
+    implementation("io.micrometer:micrometer-tracing-bridge-brave")
+    implementation("io.zipkin.reporter2:zipkin-reporter-brave")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
 }
--- order-service/build.gradle.kts
+++ order-service/build.gradle.kts
@@ -4,4 +4,8 @@ dependencies {
     implementation("org.springframework.cloud:spring-cloud-starter-config")
     implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
     implementation("org.springframework.cloud:spring-cloud-starter-circuitbreaker-resilience4j")
+    // Micrometer Tracing 의존성
+    implementation("io.micrometer:micrometer-tracing-bridge-brave")
+    implementation("io.zipkin.reporter2:zipkin-reporter-brave")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
 }
